cb84bbdd10da05be4b82d614dd9ceb1521976c6c
Simon <contact_at_saimon.org>
2010-09-20 23:09:23 +0200

    add files + gitignore

--- bin/firevacuum.sh
+++ bin/firevacuum.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 pgrep -x firefox -U $(id -u) > /dev/null && echo “Arrêtez Firefox !” && exit 1
-for i in `find ~/.mozilla -name \*.sqlite`; do sqlite3 $i vacuum; done
-for i in `find ~/.mozilla -name \*.sqlite`; do sqlite3 $i reindex; done
+for i in `find ~/.mozilla -name \*.sqlite`; do
+   echo ":: processing $i ..."
+   sqlite3 $i vacuum
+   sqlite3 $i reindex
+done
