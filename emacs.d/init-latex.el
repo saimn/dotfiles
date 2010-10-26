656099420d5aa1c6e4d26458bbc3c2f236c5e5c1
Simon <contact@saimon.org>
2010-10-26 18:58:34 +0200

    emacs - auctex conf

--- emacs.d/init-latex.el
+++ emacs.d/init-latex.el
@@ -1,9 +1,12 @@
 (load "auctex.el" nil t t)
-;; (load "preview-latex.el" nil t t)
+(load "preview-latex.el" nil t t)
 
 (defun my-tex-mode-hook ()
   (setq TeX-auto-save t)
   (setq TeX-parse-self t)
+  (setq TeX-open-quote “«~”)
+  (setq TeX-close-quote “~»”)
+  (auto-fill-mode t)
   (setq reftex-plug-into-AUCTeX t)
   (setq reftex-plug-into-AUCTeX t)
   ;; generate PDF files using pdflatex by default
@@ -11,7 +14,6 @@
   (setq-default TeX-master nil))
 
 (add-hook 'LaTeX-mode-hook 'my-tex-mode-hook)
-(add-hook 'LaTeX-mode-hook 'auto-fill-mode)
 (add-hook 'LaTeX-mode-hook 'flyspell-mode)
 (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
 (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
@@ -21,5 +23,14 @@
                 ("\\.sty\\'" . latex-mode)
                 ("\\.cls\\'" . latex-mode))
               auto-mode-alist))
+
+(setq TeX-output-view-style (quote (
+                                    (“^pdf$” “.” “zathura %o”)
+                                    (“^ps$” “.” “evince %o”)
+                                    (“^dvi$” “.” “xdvi %o”)
+                                    )))
+(setq tex-dvi-view-command “xdvi”)
+(setq tex-dvi-print-command “dvips”)
+(setq tex-alt-dvi-print-command “dvips”)
 
 (provide 'init-latex)
