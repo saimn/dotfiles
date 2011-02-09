52f58ee8100180a27cda239e72827049e1169629
Simon <contact@saimon.org>
2011-02-09 09:57:26 +0100

    misc

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -22,7 +22,7 @@
 ;;----------------------------------------------------------------------
 
 ;; Turn off mouse interface early in startup to avoid momentary display
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+;; (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
