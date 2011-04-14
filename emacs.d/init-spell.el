7b0364deb36e9f8047dd82128cf605c905d2c427
Simon <contact@saimon.org>
2011-04-15 00:52:18 +0200

    emacs - add muttrc mode

--- emacs.d/init-spell.el
+++ emacs.d/init-spell.el
@@ -18,22 +18,22 @@
       ispell-dictionary "francais"
       ispell-extra-args '("--sug-mode=ultra" "--run-together"))
 
-(dolist (hook '(lisp-mode-hook
-                emacs-lisp-mode-hook
-                ruby-mode-hook
-                yaml-mode
-                python-mode-hook
-                shell-mode-hook
-                php-mode-hook
-                css-mode-hook
-                haskell-mode-hook
-                nxml-mode-hook
-                perl-mode-hook
-                java-mode-hook
-                c-mode-hook
-                c++-mode-hook
-                erlang-mode-hook))
-  (add-hook hook 'flyspell-prog-mode))
+;; (dolist (hook '(lisp-mode-hook
+;;                 emacs-lisp-mode-hook
+;;                 ruby-mode-hook
+;;                 yaml-mode
+;;                 python-mode-hook
+;;                 shell-mode-hook
+;;                 php-mode-hook
+;;                 css-mode-hook
+;;                 haskell-mode-hook
+;;                 nxml-mode-hook
+;;                 perl-mode-hook
+;;                 java-mode-hook
+;;                 c-mode-hook
+;;                 c++-mode-hook
+;;                 erlang-mode-hook))
+;;   (add-hook hook 'flyspell-prog-mode))
 
 ;; (add-hook 'text-mode-hook 'flyspell-mode)
 
