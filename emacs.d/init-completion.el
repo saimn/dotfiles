13f1a7a5780a6fb78b3a08a4ca760840157e1843
Simon <contact@saimon.org>
2011-09-15 10:30:00 +0200

    emacs - anything conf

--- emacs.d/init-completion.el
+++ emacs.d/init-completion.el
@@ -23,6 +23,9 @@
  )
 ;(setq ido-file-extensions-order '(".org" ".txt" ".py" ".el" ".ini" ".cfg"))
 
+;;----------------------------------------------------------------------
+;; Anything
+;; see http://emacs-fu.blogspot.com/2011/09/finding-just-about-anything.html
 (require 'anything-config)
 (defun my-anything ()
   (interactive)
@@ -37,6 +40,30 @@
      anything-c-source-locate)
    " *my-anything*"))
 (global-set-key (kbd "M-X") 'my-anything)
+
+(global-set-key (kbd "C-x b")
+  (lambda() (interactive)
+    (anything
+     :prompt "Switch to: "
+     :candidate-number-limit 10                 ;; up to 10 of each
+     :sources
+     '( anything-c-source-buffers               ;; buffers
+        anything-c-source-recentf               ;; recent files
+        anything-c-source-bookmarks             ;; bookmarks
+        anything-c-source-files-in-current-dir+ ;; current dir
+        anything-c-source-locate))))            ;; use 'locate'
+
+(global-set-key (kbd "C-c I")  ;; i -> info
+  (lambda () (interactive)
+    (anything
+      :prompt "Info about: "
+      :candidate-number-limit 3
+      :sources
+      '( anything-c-source-info-libc             ;; glibc docs
+         anything-c-source-man-pages             ;; man pages
+         anything-c-source-info-emacs))))        ;; emacs
+
+;;----------------------------------------------------------------------
 
 (require 'smart-tab)
 
