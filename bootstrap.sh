c760140c2b0e3b0768def59a4b028f5d535906e7
Simon <contact@saimon.org>
2011-09-23 23:05:26 +0200

    bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,8 @@
 # -*- coding: UTF8 -*-
 
 #mkdir -p lib/hg
-mkdir -p lib/python
-mkdir -p lib/virtualenvs
+mkdir -p $HOME/lib/python
+mkdir -p $HOME/lib/virtualenvs
 #mkdir bin
 #mkdir src
 
@@ -19,6 +19,7 @@ do
     ln -s $HOME/lib/dotfiles/$i $HOME/.$i
 done
 
+rm ~/.config/zathura ~/.config/awesome ~/bin
 ln -s $HOME/lib/dotfiles/bin/ $HOME/bin
 ln -s $HOME/lib/dotfiles/awesome/ $HOME/.config/awesome
 ln -s $HOME/lib/dotfiles/zathura/ $HOME/.config/zathura
@@ -30,18 +31,27 @@ mkdir -p $HOME/lib/dotfiles/emacs.d/backup-files/
 
 #{{{ ZSH
 git clone git@github.com:saimn/oh-my-zsh.git ~/lib/oh-my-zsh
+rm ~/.zshenv ~/.zshrc
 ln -s ~/lib/oh-my-zsh/zshrc ~/.zshrc
 ln -s ~/lib/oh-my-zsh/zshenv ~/.zshenv
 #}}}
 
 # {{{ Mail
 cd
-rm .mutt .goobookrc .mairixrc .msmtprc
+rm .mutt .goobookrc .mairixrc .msmtprc .offlineimaprc .fdm.conf
+ln -s ~/lib/mail/offlineimaprc .offlineimaprc
 ln -s ~/lib/mail/goobookrc .goobookrc
 ln -s ~/lib/mail/mairixrc .mairixrc
 ln -s ~/lib/mail/msmtprc .msmtprc
+ln -s ~/lib/mail/fdm.conf .fdm.conf
 ln -s ~/lib/mail/mutt .mutt
 # }}}
+
+
+#{{{ Other repos
+git clone git@github.com:saimn/color-theme-tangotango.git ~/lib/color-theme-tangotango
+git clone git@github.com:saimn/color-theme-wombat.git ~/lib/color-theme-wombat
+#}}}
 
 #git clone https://github.com/pypa/pip.git ~/lib/python/pip
 #cd ~/lib/python/pip
