1f05376270e8d72b6511a2d8fd3ee457223b3f1e
Simon <contact@saimon.org>
2012-02-16 16:46:23 +0100

    awesome - add numlockx

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -769,6 +769,7 @@ if host == "goudes" then
 elseif host == "fireball" then
    run_once("/usr/libexec/gnome-settings-daemon")
    run_once("pulseaudio", "--start")
+   run_once("numlockx on")
    run_once("urxvt256c-mld", "-q -f -o", "urxvt256c-mld -q -f -o")
    run_once("gpaste", nil, "/usr/libexec/gpaste/gpasted")
 end
