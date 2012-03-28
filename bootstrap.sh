b297a760f58b8bfa23ccf637e4a268e9ee55a14e
Simon <contact@saimon.org>
2012-03-29 00:47:35 +0200

    bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
 
-#mkdir -p lib/hg
-mkdir -p $HOME/lib/python
-mkdir -p $HOME/lib/virtualenvs
-#mkdir bin
-#mkdir src
-
 #{{{ Dotfiles
 git clone git@github.com:saimn/dotfiles.git ~/lib/dotfiles
 
-for i in "ackrc" "bashrc" "cliverc" "ctags" "dzen" "emacs.d" "gitconfig" \
+for i in "ackrc" "bashrc" "cliverc" "ctags" "dzen" "gitconfig" \
     "gmrunrc" "hgrc" "lessfilter" "mailcap" "pentadactyl" "pentadactylrc" \
     "pythonrc" "screenrc" "tmux.conf" "vim" "vimrc" "xbindkeysrc" \
     "Xdefaults" "Xmodmap" "xprofile" "xsession"
@@ -24,13 +18,7 @@ ln -s $HOME/lib/dotfiles/bin/ $HOME/bin
 ln -s $HOME/lib/dotfiles/awesome/ $HOME/.config/awesome
 ln -s $HOME/lib/dotfiles/zathura/ $HOME/.config/zathura
 
-mkdir -p $HOME/lib/dotfiles/vim/backup/undo
-mkdir -p $HOME/lib/dotfiles/emacs.d/backup-files/
-
-rm $HOME/.ipython/ipy_user_conf.py
-rm $HOME/.ipython/virtualenv.py
-ln -s $HOME/lib/dotfiles/ipython/ipy_user_conf.py $HOME/.ipython/
-ln -s $HOME/lib/dotfiles/ipython/virtualenv.py $HOME/.ipython/
+mkdir -p $HOME/lib/dotfiles/vim/tmp/{backup,swap,undo}
 
 #}}}
 
@@ -40,6 +28,12 @@ rm ~/.zshenv ~/.zshrc
 ln -s ~/lib/oh-my-zsh/zshrc ~/.zshrc
 ln -s ~/lib/oh-my-zsh/zshenv ~/.zshenv
 #}}}
+
+# {{{ Emacs
+git clone git@github.com:saimn/dotemacs.git ~/lib/dotemacs/
+ln -s ~/lib/dotemacs ~/.emacs.d
+mkdir -p $HOME/lib/dotfiles/emacs.d/backup-files/
+# }}}
 
 # {{{ Mail
 cd
@@ -52,15 +46,24 @@ ln -s ~/lib/mail/fdm.conf .fdm.conf
 ln -s ~/lib/mail/mutt .mutt
 # }}}
 
+# {{{ Python
+mkdir -p $HOME/lib/python
+mkdir -p $HOME/lib/virtualenvs
+
+rm $HOME/.config/ipython/profile_default/ipython_config.py
+ln -s $HOME/lib/dotfiles/ipython/ipython_config.py \
+    $HOME/.config/ipython/profile_default/ipython_config.py
+# }}}
 
 #{{{ Other repos
-hg clone https://bitbucket.org/thayerwilliams/vimbrant ~/lib/vimbrant/
-git clone git@github.com:saimn/color-theme-tangotango.git ~/lib/color-theme-tangotango
-git clone git@github.com:saimn/color-theme-wombat.git ~/lib/color-theme-wombat
-git clone https://github.com/gfxmonk/supergenpass.git ~/lib/supergenpass/
-git clone https://github.com/jsmits/github-cli.git ~/lib/github-cli/
-git clone https://github.com/apenwarr/sshuttle.git ~/lib/sshuttle/
-#}}}
+git clone git@github.com:saimn/color-theme-tangotango.git   ~/lib/color-theme-tangotango
+git clone git@github.com:saimn/color-theme-wombat.git       ~/lib/color-theme-wombat
+git clone https://github.com/jsmits/github-cli.git          ~/lib/github-cli/
+git clone git://gitorious.org/mutt-notmuch/mutt-notmuch.git ~/lib/mutt-notmuch/
+git clone https://github.com/apenwarr/sshuttle.git          ~/lib/sshuttle/
+git clone https://github.com/gfxmonk/supergenpass.git       ~/lib/supergenpass/
+hg  clone https://bitbucket.org/thayerwilliams/vimbrant     ~/lib/vimbrant/
+# }}}
 
 #git clone https://github.com/pypa/pip.git ~/lib/python/pip
 #cd ~/lib/python/pip
