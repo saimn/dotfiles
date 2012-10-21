8cf9df549c0a09aa47dde12eb710364556bd5ffe
Simon <contact@saimon.org>
2012-10-21 13:14:44 +0200

    scripts cleanup: remove exif_rename, generate_exif_thumbnail, makeMakefile, opensyd-logout, window_manager, msearch, pylendar, pypres, screenshot

--- bin/lmt-vicious.sh
+++ bin/lmt-vicious.sh
@@ -5,14 +5,14 @@
 # Script for laptop-mode start-stop-programs module
 
 vicious_start() {
-    /bin/su - user -c "/bin/echo 'vicious.activate()' | /usr/bin/awesome-client"
+    /bin/su - simon -c "/bin/echo 'vicious.activate()' | /usr/bin/awesome-client"
     /bin/echo "Activated Vicious widgets for Awesome window manager."
 }
 
 vicious_stop() {
-    /bin/su - user -c "/bin/echo 'vicious.suspend()' | /usr/bin/awesome-client"
+    /bin/su - simon -c "/bin/echo 'vicious.suspend()' | /usr/bin/awesome-client"
     # Some widgets are important while running on battery, BAT widget in particular
-    /bin/su - user -c "/bin/echo 'vicious.activate(batwidget)' | /usr/bin/awesome-client"
+    /bin/su - simon -c "/bin/echo 'vicious.activate(batwidget)' | /usr/bin/awesome-client"
     /bin/echo "Suspended Vicious widgets for Awesome window manager."
 }
 
