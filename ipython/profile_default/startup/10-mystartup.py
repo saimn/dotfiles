76f2043fd18388477198cb56cc89725294eac332
Simon Conseil <contact@saimon.org>
2015-10-08 09:16:00 +0200

    astropy startup

--- ipython/profile_default/startup/10-mystartup.py
+++ ipython/profile_default/startup/10-mystartup.py
@@ -6,15 +6,15 @@ import numpy as np
 ip = get_ipython()
 
 
-def import_astropy(self, arg):
+def _import_astropy(self, arg):
     ip.ex('import astropy')
-    ip.ex('print(astropy.__version__)')
-    ip.ex('from astropy.io import ascii')
-    ip.ex('from astropy import table')
+    ip.ex('print("Astropy", astropy.__version__)')
+    ip.ex('from astropy.io import ascii, fits')
+    # ip.ex('from astropy import table')
     ip.ex('from astropy.table import Table, QTable, Column, MaskedColumn')
-    ip.ex('from astropy.table.table_helpers import TimingTables, simple_table, complex_table')
-    ip.ex('from astropy.time import Time, TimeDelta')
-    ip.ex('from astropy.coordinates import SkyCoord, ICRS, FK4, FK5')
+    # ip.ex('from astropy.table.table_helpers import TimingTables, simple_table, complex_table')
+    # ip.ex('from astropy.time import Time, TimeDelta')
+    # ip.ex('from astropy.coordinates import SkyCoord, ICRS, FK4, FK5')
     ip.ex('import astropy.units as u')
     try:
         import line_profiler
@@ -22,4 +22,4 @@ def import_astropy(self, arg):
     except ImportError:
         pass
 
-ip.define_magic('astro', import_astropy)
+ip.define_magic('astro', _import_astropy)
