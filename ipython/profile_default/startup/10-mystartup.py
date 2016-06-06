221f0f436db2dbb45ecd61795651de552b16322f
Simon Conseil <contact@saimon.org>
2016-06-06 12:27:02 +0200

    python - shorcuts, requirements

--- ipython/profile_default/startup/10-mystartup.py
+++ ipython/profile_default/startup/10-mystartup.py
@@ -19,14 +19,12 @@ def _import_astropy(self, arg):
     _import_numpy(self, arg)
     ip.ex('import astropy')
     ip.ex('print("Astropy", astropy.__version__)')
+    ip.ex('import astropy.units as u')
     ip.ex('from astropy.io import ascii, fits')
     ip.ex('from astropy.wcs import WCS')
-    # ip.ex('from astropy import table')
     ip.ex('from astropy.table import Table, Column, MaskedColumn')
-    # ip.ex('from astropy.table.table_helpers import TimingTables, simple_table, complex_table')
     # ip.ex('from astropy.time import Time, TimeDelta')
     # ip.ex('from astropy.coordinates import SkyCoord, ICRS, FK4, FK5')
-    ip.ex('import astropy.units as u')
 
 
 def _import_mpdaf(self, arg):
@@ -38,6 +36,12 @@ def _import_mpdaf(self, arg):
     ip.ex('from mpdaf.sdetect import Source')
 
 
+def _import_imu(self, arg):
+    ip.ex('import ipython_memory_usage.ipython_memory_usage as imu')
+    ip.ex('imu.start_watching_memory()')
+
+
 ip.define_magic('astro', _import_astropy)
 ip.define_magic('mpdaf', _import_mpdaf)
 ip.define_magic('numpy', _import_numpy)
+ip.define_magic('imu', _import_imu)
