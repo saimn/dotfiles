e55ef77512051de19bd2ce50b892c1d6ad4a2469
Simon Conseil <contact@saimon.org>
2016-07-18 10:51:17 +0200

    Make IPython startup file compatible with version 5

--- ipython/profile_default/startup/10-mystartup.py
+++ ipython/profile_default/startup/10-mystartup.py
@@ -1,22 +1,25 @@
 #   ~/.ipython/profile_default/startup/10-mystartup.py
 # from https://gist.github.com/taldcroft/547c0b6e0ae15e0c970d
 
+from IPython.core.magic import register_line_magic
+
 ip = get_ipython()
 
-try:
-    import line_profiler
-    ip.define_magic('lprun', line_profiler.magic_lprun)
-except ImportError:
-    pass
+# try:
+#     import line_profiler
+#     ip.define_magic('lprun', line_profiler.magic_lprun)
+# except ImportError:
+#     pass
 
 
-def _import_numpy(self, arg):
+def _import_numpy():
     ip.ex('import numpy as np')
     ip.ex('from numpy import ma')
+    ip.ex('print("Numpy", np.__version__)')
 
 
-def _import_astropy(self, arg):
-    _import_numpy(self, arg)
+def _import_astropy():
+    _import_numpy()
     ip.ex('import astropy')
     ip.ex('print("Astropy", astropy.__version__)')
     ip.ex('import astropy.units as u')
@@ -27,21 +30,37 @@ def _import_astropy(self, arg):
     # ip.ex('from astropy.coordinates import SkyCoord, ICRS, FK4, FK5')
 
 
-def _import_mpdaf(self, arg):
-    _import_astropy(self, arg)
+def _import_mpdaf():
+    _import_astropy()
     ip.ex('import mpdaf')
     ip.ex('print("MPDAF", mpdaf.__version__)')
     ip.ex('from mpdaf.obj import Cube, Image, Spectrum, WCS, WaveCoord')
     ip.ex('from mpdaf.drs import PixTable')
-    ip.ex('from mpdaf.sdetect import Source')
+    ip.ex('from mpdaf.sdetect import Source, Catalog')
 
 
-def _import_imu(self, arg):
+@register_line_magic
+def numpy(line):
+    """Import Numpy"""
+    _import_numpy()
+
+
+@register_line_magic
+def astropy(line):
+    """Import Astropy"""
+    _import_astropy()
+
+
+@register_line_magic
+def mpdaf(line):
+    """Import MPDAF"""
+    _import_mpdaf()
+
+
+@register_line_magic
+def imu():
     ip.ex('import ipython_memory_usage.ipython_memory_usage as imu')
     ip.ex('imu.start_watching_memory()')
 
 
-ip.define_magic('astro', _import_astropy)
-ip.define_magic('mpdaf', _import_mpdaf)
-ip.define_magic('numpy', _import_numpy)
-ip.define_magic('imu', _import_imu)
+del numpy, astropy, mpdaf, imu
