000ff85c982744d241c3f073be91343d4159a64c
Simon Conseil <simon.conseil@univ-lyon1.fr>
2017-03-02 12:35:00 +0100

    disable line_profiler extension

--- ipython/extensions/line_profiler_ext.py
+++ ipython/extensions/line_profiler_ext.py
@@ -3,6 +3,6 @@ try:
 except ImportError:
     line_profiler = False
 
-def load_ipython_extension(ip):
-    if line_profiler:
-        ip.define_magic('lprun', line_profiler.magic_lprun)
+# def load_ipython_extension(ip):
+#     if line_profiler:
+#         ip.define_magic('lprun', line_profiler.magic_lprun)
