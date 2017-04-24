553589420ef46b2ddcbecf2b954de69ffcaca97c
Simon Conseil <contact@saimon.org>
2017-04-24 14:27:36 +0200

    Remove ipython/extensions

--- ipython/extensions/line_profiler_ext.py
+++
@@ -1,8 +0,0 @@
-try:
-    import line_profiler
-except ImportError:
-    line_profiler = False
-
-# def load_ipython_extension(ip):
-#     if line_profiler:
-#         ip.define_magic('lprun', line_profiler.magic_lprun)
