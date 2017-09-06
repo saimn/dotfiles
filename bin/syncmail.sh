f049281ae9e90e79a0eb5ddd9e97f2976f9d8562
Simon Conseil <contact@saimon.org>
2017-09-06 22:34:13 +0200

    update syncmail and add systemd services

--- bin/syncmail.sh
+++ bin/syncmail.sh
@@ -1,13 +1,38 @@
-#!/bin/bash
+#!/bin/sh
 # -*- coding: utf-8 -*-
 
-while true;
-do
-    echo -n "`date +%H:%M` Syncing... "
-    mbsync -q -a && echo OK
+set -eu
+
+verbosity=""
+mbox="-a"
+
+while true; do
+    case "${1:-}" in
+    --loop) loop=1; shift;;
+    --quick) mbox="INBOX"; shift;;
+    -V) verbosity="-V"; shift;;
+    -q) verbosity="-q"; shift;;
+    *)  break ;;
+    esac
+done
+
+sync() {
+    mbsync $verbosity $mbox && echo OK
     mu index -q --maildir=~/Mail
     mu find --clearlinks --format=links --linksdir=~/Mail/INBOX \
         m:'/cral/INBOX' OR m:'/sconseil/INBOX' OR m:'/saimon/INBOX'
-    sleep 5m
-done
+}
 
+
+if [ ${loop:-0} -eq 1 ]; then
+    while true;
+    do
+        echo -n "`date +%H:%M` Syncing... "
+        sync
+        sleep 5m
+    done
+else
+    sync
+fi
+
+exit
