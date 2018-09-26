c83c3aecb8fef52ed34d42dff7433db017b7264d
Simon Conseil <simon.conseil@univ-lyon1.fr>
2018-09-26 10:49:14 +0200

    jupyterlab

--- python/jupyterlab-extensions.sh
+++ python/jupyterlab-extensions.sh
@@ -1,4 +1,26 @@
 #!/bin/sh
 
-jupyter labextension install jupyterlab-toc
+pip install -r ./jupyter-requirements.txt
+
 jupyter labextension install @jupyter-widgets/jupyterlab-manager
+
+jupyter labextension install @jupyterlab/github
+jupyter labextension install @jupyterlab/shortcutui
+jupyter labextension install @jupyterlab/toc
+# jupyter labextension install jupyterlab_vim
+
+jupyter labextension install @ijmbarr/jupyterlab_spellchecker
+jupyter labextension install @mflevine/jupyterlab_html
+
+# git
+jupyter labextension install @jupyterlab/git
+pip install jupyterlab-git
+jupyter serverextension enable --py jupyterlab_git
+
+# LaTeX
+pip install jupyterlab_latex
+jupyter labextension install @jupyterlab/latex
+
+# Matplotlib
+pip install ipympl
+jupyter labextension install jupyter-matplotlib
