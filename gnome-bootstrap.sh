4fb5cef361034f33ebf6a736f73f355923947f6d
Simon Conseil <sconseil@gemini.edu>
2019-10-14 10:02:03 -0300

    use full path for gsettings

--- gnome-bootstrap.sh
+++ gnome-bootstrap.sh
@@ -3,44 +3,44 @@
 # Bootstrap script to configure a fresh gnome-shell install
 #
 
-gsettings set org.gnome.desktop.interface clock-show-date true
+/usr/bin/gsettings set org.gnome.desktop.interface clock-show-date true
 
 # evince
-gsettings set org.gnome.Evince.Default continuous false
-gsettings set org.gnome.Evince.Default show-sidebar false
+/usr/bin/gsettings set org.gnome.Evince.Default continuous false
+/usr/bin/gsettings set org.gnome.Evince.Default show-sidebar false
 
 # dock
-# gsettings set org.gnome.shell.extensions.dock position 'left'
+# /usr/bin/gsettings set org.gnome.shell.extensions.dock position 'left'
 
 # default apps
-# gsettings set org.gnome.desktop.default-applications.terminal exec urxvtc
-# gsettings set org.gnome.desktop.default-applications.terminal exec-arg "'-e'"
+# /usr/bin/gsettings set org.gnome.desktop.default-applications.terminal exec urxvtc
+# /usr/bin/gsettings set org.gnome.desktop.default-applications.terminal exec-arg "'-e'"
 
 # keybindings
-gsettings set org.gnome.desktop.wm.keybindings maximize "['<Super>m']"
-gsettings set org.gnome.desktop.wm.keybindings panel-run-dialog "['<Super>F2']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>2']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>3']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>4']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "['<Super>Down']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "['<Super>Up']"
-gsettings set org.gnome.desktop.wm.keybindings toggle-fullscreen "['<Super>f']"
-gsettings set org.gnome.desktop.wm.keybindings toggle-maximized "['<Super>space']"
-gsettings set org.gnome.mutter.keybindings toggle-tiled-left "['<Super>Left']"
-gsettings set org.gnome.mutter.keybindings toggle-tiled-right "['<Super>Right']"
-gsettings set org.gnome.shell.keybindings focus-active-notification "['<Super>u']"
-gsettings set org.gnome.shell.keybindings toggle-application-view "['<Super>a']"
-gsettings set org.gnome.shell.keybindings toggle-message-tray "['<Super>n']"
-gsettings set org.gnome.shell.keybindings toggle-overview "['<Super>s']"
+/usr/bin/gsettings set org.gnome.desktop.wm.keybindings maximize "['<Super>m']"
+/usr/bin/gsettings set org.gnome.desktop.wm.keybindings panel-run-dialog "['<Super>F2']"
+/usr/bin/gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']"
+/usr/bin/gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>2']"
+/usr/bin/gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>3']"
+/usr/bin/gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>4']"
+/usr/bin/gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "['<Super>Down']"
+/usr/bin/gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "['<Super>Up']"
+/usr/bin/gsettings set org.gnome.desktop.wm.keybindings toggle-fullscreen "['<Super>f']"
+/usr/bin/gsettings set org.gnome.desktop.wm.keybindings toggle-maximized "['<Super>space']"
+/usr/bin/gsettings set org.gnome.mutter.keybindings toggle-tiled-left "['<Super>Left']"
+/usr/bin/gsettings set org.gnome.mutter.keybindings toggle-tiled-right "['<Super>Right']"
+/usr/bin/gsettings set org.gnome.shell.keybindings focus-active-notification "['<Super>u']"
+/usr/bin/gsettings set org.gnome.shell.keybindings toggle-application-view "['<Super>a']"
+/usr/bin/gsettings set org.gnome.shell.keybindings toggle-message-tray "['<Super>n']"
+/usr/bin/gsettings set org.gnome.shell.keybindings toggle-overview "['<Super>s']"
 
 
 
 # meteo
-# gsettings set org.gnome.shell.extensions.weather city Marseille
-# gsettings set org.gnome.shell.extensions.weather unit celsius
-# gsettings set org.gnome.shell.extensions.weather woeid 610264
-# gsettings set org.gnome.shell.extensions.weather use-symbolic-icons true
+# /usr/bin/gsettings set org.gnome.shell.extensions.weather city Marseille
+# /usr/bin/gsettings set org.gnome.shell.extensions.weather unit celsius
+# /usr/bin/gsettings set org.gnome.shell.extensions.weather woeid 610264
+# /usr/bin/gsettings set org.gnome.shell.extensions.weather use-symbolic-icons true
 
 # themes
 # cd ~/.themes
