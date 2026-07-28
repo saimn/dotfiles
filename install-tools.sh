d068b0d74fddbf8349b668ae8dfbb3d438494850
Simon Conseil <simon.conseil@univ-lyon1.fr>
2026-07-28 17:28:41 +0200

    replace bin with pixi

--- install-tools.sh
+++ install-tools.sh
@@ -1,15 +1,41 @@
 #!/bin/bash
 
-bin completion zsh > ~/.zsh-complete/_bin
+#
+# bin
+#
 
-bin install github.com/sharkdp/fd
+# bin completion zsh > ~/.zsh-complete/_bin
+# bin install github.com/astral-sh/uv
+# bin install github.com/BurntSushi/ripgrep
+# bin install github.com/neovim/neovim
+# bin install github.com/sharkdp/fd
+# bin install github.com/sxyazi/yazi
+
+#
+# pixi
+#
+
+# curl -fsSL https://pixi.sh/install.sh | sh
+
+pixi global install \
+    bat \
+    bear \
+    clang clang-tools \
+    fd-find \
+    fzf \
+    nodejs \
+    nvim pynvim \
+    pre-commit \
+    ripgrep \
+    ruff \
+    universal-ctags \
+    uv \
+    yazi
+
+#
+# completion
+#
+
 fd --gen-completions zsh > ~/.zsh-complete/_fd
-
-bin install github.com/BurntSushi/ripgrep
+pixi completion --shell zsh > ~/.zsh-complete/_pixi
 rg --generate complete-zsh > ~/.zsh-complete/_rg
-
-bin install github.com/sxyazi/yazi
-
-bin install github.com/neovim/neovim
-
-bin install github.com/astral-sh/uv
